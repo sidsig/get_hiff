43aba902fe7f497bef302d70bb1252416c90504c
sidsig <siddharth.sigtia@gmail.com>
2014-03-17 13:32:57 +0000

    Fixing Apocrita job files.

--- create_files_chrisantha.py
+++ create_files_chrisantha.py
@@ -18,14 +18,13 @@ for pop_size in [500,600,700,1000]:
                                                         for hiddens in [10,25,50,100]:
                                                                 for corruption_level in [0.1,0.2,0.5,0.8,0.9]:
                                                                         for use_good_strings in [1]:
-                                                                                for sample_sd in [0.001,0.01,0.001]
+                                                                                for sample_sd in [0.001,0.01,0.001]:
                                                                                         for trials in range(0,5):
-                                                                                                command = "python2.6 rbm_solver.py {0} {1} 20 {2} {3} {4} {5} {6} {7} {8} {9} {10} 20 {11} {12}".format(
-                                                                                                        pop_size,lim_percentage,num_epochs,lr,online_training,unique_training,sample_rate,
-                                                                                                        hiddens,corruption_level,use_good_strings,sample_sd,trials)
+                                                                                                
+                                                                                                command = "python2.6 rbm_solver.py {0} {1} 20 {2} {3} {4} {5} {6} {7} {8} {9} 20 {10} {11}".format(
+                                                                                                        pop_size,lim_percentage,num_epochs,lr,online_training,unique_training,sample_rate,hiddens,corruption_level,use_good_strings,sample_sd,trials)
                                                                                                 f_text = create_text(command)
-                                                                                                f = open("ae_shere_{0}_{1}_20_{2}_{3}_{4}_{5}_{6}_{7}_{8}_{9}_{10}_20_{11}_{12}.job".format(
-                                                                                                        pop_size,lim_percentage,num_epochs,lr,online_training,unique_training,sample_rate,
-                                                                                                        hiddens,corruption_level,use_good_strings,sample_sd,trials),"w")
+                                                                                                f = open("ae_shere_{0}_{1}_20_{2}_{3}_{4}_{5}_{6}_{7}_{8}_{9}_20_{10}_{11}.job".format(
+                                                                                                        pop_size,lim_percentage,num_epochs,lr,online_training,unique_training,sample_rate,hiddens,corruption_level,use_good_strings,sample_sd,trials),"w")
                                                                                                 f.write(f_text)
 
